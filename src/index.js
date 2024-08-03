b07c611e17200e7492f9264b8028a65c0d9b3771
Tankred Hase <mail@tankredhase.de>
2024-08-03 15:40:29 +0100

    Add test code to receive

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import {
     LiquidNetwork,
     listPayments,
     removeEventListener,
+    fetchLightningLimits,
+    fetchOnchainLimits,
+    prepareReceiveOnchain,
+    receiveOnchain,
     prepareReceivePayment,
     prepareSendPayment,
     receivePayment,
@@ -29,6 +33,60 @@ export async function init() {
 
   await connect({ mnemonic, config })
   console.log("connect")
+
+  // get wallet info
+  const walletInfo = await getInfo()
+  const balanceSat = walletInfo.balanceSat
+  const pendingSendSat = walletInfo.pendingSendSat
+  const pendingReceiveSat = walletInfo.pendingReceiveSat
+  console.log(`Wallet balance: ${balanceSat}`)
+  console.log(`Wallet pending send balance: ${pendingSendSat}`)
+  console.log(`Wallet pending receive balance: ${pendingReceiveSat}`)
+
+  // Fetch the Receive limits
+  const currentLimits = await fetchLightningLimits()
+  console.log(`Minimum amount, in sats: ${currentLimits.receive.minSat}`)
+  console.log(`Maximum amount, in sats: ${currentLimits.receive.maxSat}`)
+
+  // Set the amount you wish the payer to send, which should be within the above limits
+  const prepareRes = await prepareReceivePayment({
+    payerAmountSat: 1_000
+  })
+
+  // If the fees are acceptable, continue to create the Receive Payment
+  const receiveFeesSat = prepareRes.feesSat
+  console.log(`Receive fees, in sats: ${receiveFeesSat}`)
+
+  const optionalDescription = '<description>'
+  const res = await receivePayment({
+    prepareRes,
+    description: optionalDescription
+  })
+
+  const invoice = res.invoice
+  console.log(`Invoice: ${invoice}`)
+
+  // Fetch the Onchain Receive limits
+  const currentOnchainLimits = await fetchOnchainLimits()
+  console.log(`Minimum on-chain amount, in sats: ${currentOnchainLimits.receive.minSat}`)
+  console.log(`Maximum on-chain amount, in sats: ${currentOnchainLimits.receive.maxSat}`)
+
+  // Set the amount you wish the payer to send, which should be within the above limits
+  const prepareResponse = await prepareReceiveOnchain({
+    payerAmountSat: 50_000
+  })
+
+  // If the fees are acceptable, continue to create the Onchain Receive Payment
+  const receiveOnchainFeesSat = prepareResponse.feesSat
+  console.log(`Receive on-chain fees, in sats: ${receiveOnchainFeesSat}`)
+
+  const receiveOnchainResponse = await receiveOnchain(prepareResponse)
+
+  // Send your funds to the below bitcoin address
+  const address = receiveOnchainResponse.address
+  const bip21 = receiveOnchainResponse.bip21
+  console.log(`Address: ${address}`)
+  console.log(`Bip21: ${bip21}`)
 }
 
 init();
