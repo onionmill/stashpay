b9a42e66438c6f599baddc0f2cc2508714e7028c
Tankred Hase <mail@tankredhase.de>
2024-08-07 18:32:31 +0100

    Patch metro.config.js for node-libs-react-native

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,10 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  resolver: {
+    extraNodeModules: require('node-libs-react-native'),
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
