a2aad8bad2d1a855334aa19a8cf1acdc92e8b1b7
Tankred Hase <mail@tankredhase.com>
2024-08-29 13:43:15 +0100

    Copy files from fresh react-native@0.75.2 template

--- ios/stashpayTests/stashpayTests.m
+++ ios/stashpayTests/stashpayTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface stashpayTests : XCTestCase
+@interface StashPayTests : XCTestCase
 
 @end
 
-@implementation stashpayTests
+@implementation StashPayTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
