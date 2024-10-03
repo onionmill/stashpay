c5840a0d1fca4c8f5d0806591767522c540e25bf
Tankred Hase <mail@tankredhase.com>
2024-10-03 14:07:59 +0100

    Cleanup send-amount screen code

--- src/screen/send-amount.js
+++ src/screen/send-amount.js
@@ -2,45 +2,29 @@ import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import {observer} from 'mobx-react';
 
-import {Text} from '../component/text';
+import {H1Text} from '../component/text';
 import {TextInput} from '../component/input';
 import {PillButton} from '../component/button';
+import {MainContent, Spacer} from '../component/layout';
 import {Background} from '../component/background';
-import {font} from '../component/style';
 
 import store from '../store';
 import * as send from '../action/send';
 
 const styles = StyleSheet.create({
-  wrapper: {
-    padding: 10,
-  },
-  paramsWrapper: {
-    flex: 1,
-  },
-  label: {
-    marginTop: 20,
-  },
   input: {
-    fontSize: font.sizeXL,
-    height: font.lineHeightXL,
-  },
-  addressText: {
-    marginVertical: 15,
-    fontSize: font.sizeSub,
-    lineHeight: font.lineHeightSub,
+    marginTop: 10,
   },
   btnWrapper: {
     marginTop: 10,
-    marginBottom: 30,
     height: 150,
   },
 });
 
 const SendAmountScreen = () => (
-  <Background style={styles.wrapper}>
-    <View style={styles.paramsWrapper}>
-      <Text style={styles.label}>Amount (sats):</Text>
+  <Background>
+    <MainContent>
+      <H1Text>Amount (sats)</H1Text>
       <TextInput
         placeholder="1000 minumum"
         keyboardType="number-pad"
@@ -49,10 +33,11 @@ const SendAmountScreen = () => (
         value={store.send.value}
         onChangeText={value => send.setAmount(value)}
       />
-    </View>
-    <View style={styles.btnWrapper}>
-      <PillButton onPress={() => send.validateAmount()}>Review</PillButton>
-    </View>
+      <Spacer />
+      <View style={styles.btnWrapper}>
+        <PillButton onPress={() => send.validateAmount()}>Review</PillButton>
+      </View>
+    </MainContent>
   </Background>
 );
 
